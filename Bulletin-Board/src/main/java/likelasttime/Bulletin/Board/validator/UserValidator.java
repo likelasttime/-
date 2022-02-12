72ce9a613762c92973ae47d01354a54927262ff2
likelasttime <a50070863@gmail.com>
2022-02-12 20:49:55 +0900

    :bug: Optional의 get 메소드 에러 해결
    ofNullable 메소드에 null이 될 수 있는 값을 get으로 가져온 게 문제

--- Bulletin-Board/src/main/java/likelasttime/Bulletin/Board/validator/UserValidator.java
+++ Bulletin-Board/src/main/java/likelasttime/Bulletin/Board/validator/UserValidator.java
@@ -24,14 +24,14 @@ public class UserValidator implements Validator {
     @Override
     public void validate(Object object, Errors errors){
         UserRequestDto userRequestDto=(UserRequestDto) object;
-        Optional<User> user=Optional.ofNullable(userRepository.findByUsername(userRequestDto.getUsername()).get());
+        Optional<User> user=userRepository.findByUsername(userRequestDto.getUsername());
 
         String email=userRequestDto.getEmail();
         String username=userRequestDto.getUsername();
         String phone=userRequestDto.getPhone();
 
         if(userRepository.existsByEmail(email)){    // 이메일 중복
-            if((user != null && !email.equals(user.get().getEmail())) || user == null) {    // 수정한 이메일 또는 가입할 이메일
+            if((user.isPresent() && !email.equals(user.get().getEmail())) || !user.isPresent()) {    // 수정한 이메일 또는 가입할 이메일
                 errors.rejectValue("email", "invalid.email"
                         , new Object[]{email}
                         , "이미 사용중인 이메일입니다.");
@@ -45,13 +45,15 @@ public class UserValidator implements Validator {
         }
 
         if(userRepository.existsByUsername(username)){    // 아이디 중복
+            if((user.isPresent() && !username.equals(user.get().getUsername())) || !user.isPresent()) {    // 수정할 아이디 또는 가입할 아이디
                 errors.rejectValue("username", "invalid.username"
                         , new Object[]{username}
                         , "이미 사용중인 아이디입니다.");
+            }
         }
 
         if(userRepository.existsByPhone(phone)){  // 연략처 중복
-            if((user != null && !phone.equals(user.get().getPhone())) || user == null) {    // 수정한 연락처 또는 가입할 연락처
+            if((user.isPresent() && !phone.equals(user.get().getPhone())) || !user.isPresent()) {    // 수정한 연락처 또는 가입할 연락처
                 errors.rejectValue("phone", "invalid.phone"
                         , new Object[]{phone}
                         , "이미 가입된 번호입니다.");
