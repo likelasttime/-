eeb340704c20ed7742eb5113953d0ff34bd57cdc
KangMinjeong <a50070863@gmail.com>
2022-07-23 22:49:53 +0900

    :white_check_mark: 캐시 삭제 테스트 추가
    @CacheEvict 어노테이션이 잘 동작하는지 확인하기 위해 삭제 테스트 작성

--- Bulletin-Board/src/test/java/RedisTest.java
+++ Bulletin-Board/src/test/java/RedisTest.java
@@ -2,6 +2,7 @@ import likelasttime.Bulletin.Board.BulletinBoardApplication;
 import likelasttime.Bulletin.Board.Repository.PostRepository;
 import likelasttime.Bulletin.Board.Service.PostService;
 import likelasttime.Bulletin.Board.domain.posts.Post;
+import likelasttime.Bulletin.Board.domain.posts.PostResponseDto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,12 +21,12 @@ public class RedisTest {
     PostRepository postRepository;
 
     @AfterEach
-    public void afterAll(){
-        postRepository.deleteAll();
+    public void afterEach(){
+        postService.deleteAll();
     }
 
     @Test
-    public void redisFindByRank(){
+    public void redisFindByRank(){  // 조회수를 기준으로 상위 10개의 게시글을 출력(캐시 사용)
         // given
         Post post=Post.builder()
                 .title("첫 번째")
@@ -37,12 +38,33 @@ public class RedisTest {
 
         // when
         postRepository.save(post);
-        List<Post> postList=postService.findByRank();
+        List<PostResponseDto> postList=postService.findByRank();
 
         // then
-        assertThat(postList.get(0).getTitle()).isEqualTo("첫 번째");
-        assertThat(postList.get(0).getContent()).isEqualTo("안녕");
-        assertThat(postList.get(0).getAuthor()).isEqualTo("im");
+        assertThat(postList.get(0).getTitle()).isEqualTo(post.getTitle());
+        assertThat(postList.get(0).getContent()).isEqualTo(post.getContent());
+        assertThat(postList.get(0).getAuthor()).isEqualTo(post.getAuthor());
+        assertThat(postList.get(0).getView()).isEqualTo(post.getView());
+        assertThat(postList.get(0).getComment_cnt()).isEqualTo(post.getComment_cnt());
+    }
 
+    @Test
+    public void redisDeletePost(){
+        // given
+        Post post=Post.builder()
+                .title("첫 번째")
+                .content("안녕")
+                .author("im")
+                .view(0)
+                .comment_cnt(0)
+                .build();
+
+        // when
+        postRepository.save(post);
+        postService.findAll();      // 전체 조회하면 캐시에 저장됨
+        postService.deletePost(post.getId());
+
+        // then
+        assertThat(postService.findAll().size()).isEqualTo(0);
     }
 }
