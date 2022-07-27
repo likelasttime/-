063c82d611eab25f21d4c1130a82fd7bacb9fbfd
KangMinjeong <a50070863@gmail.com>
2022-07-27 14:39:38 +0900

    :white_check_mark: RedisTest 수정
    테스트 코드 수정 및 update 테스트 추가

--- Bulletin-Board/src/test/java/RedisTest.java
+++ Bulletin-Board/src/test/java/RedisTest.java
@@ -2,11 +2,14 @@ import likelasttime.Bulletin.Board.BulletinBoardApplication;
 import likelasttime.Bulletin.Board.Repository.PostRepository;
 import likelasttime.Bulletin.Board.Service.PostService;
 import likelasttime.Bulletin.Board.domain.posts.Post;
+import likelasttime.Bulletin.Board.domain.posts.PostRequestDto;
 import likelasttime.Bulletin.Board.domain.posts.PostResponseDto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import java.util.List;
 
@@ -20,6 +23,12 @@ public class RedisTest {
     @Autowired
     PostRepository postRepository;
 
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
     @AfterEach
     public void afterEach(){
         postService.deleteAll();
@@ -28,24 +37,38 @@ public class RedisTest {
     @Test
     public void redisFindByRank(){  // 조회수를 기준으로 상위 10개의 게시글을 출력(캐시 사용)
         // given
-        Post post=Post.builder()
-                .title("첫 번째")
+        Post post1=Post.builder()
+                .title("두 번째")
                 .content("안녕")
                 .author("im")
-                .view(0)
+                .view(1)
                 .comment_cnt(0)
                 .build();
 
+        Post post2=Post.builder()
+                .title("첫 번째")
+                .content("안녕")
+                .author("im")
+                .view(2)
+                .comment_cnt(0)
+                .build();
+
+        postRepository.save(post1);
+        PostResponseDto postResponseDto1=modelMapper.map(post1, PostResponseDto.class);
+        redisTemplate.opsForZSet().add("findByRank", post1.getId().toString(), post1.getView());
+        redisTemplate.opsForHash().put("rankByHash", post1.getId().toString(), postResponseDto1);
+
+        postRepository.save(post2);
+        PostResponseDto postResponseDto2=modelMapper.map(post2, PostResponseDto.class);
+        redisTemplate.opsForZSet().add("findByRank", post2.getId().toString(), post2.getView());
+        redisTemplate.opsForHash().put("rankByHash", post2.getId().toString(), postResponseDto2);
+
         // when
-        postRepository.save(post);
         List<PostResponseDto> postList=postService.findByRank();
 
         // then
-        assertThat(postList.get(0).getTitle()).isEqualTo(post.getTitle());
-        assertThat(postList.get(0).getContent()).isEqualTo(post.getContent());
-        assertThat(postList.get(0).getAuthor()).isEqualTo(post.getAuthor());
-        assertThat(postList.get(0).getView()).isEqualTo(post.getView());
-        assertThat(postList.get(0).getComment_cnt()).isEqualTo(post.getComment_cnt());
+        assertThat(postList.get(0).getTitle()).isEqualTo(postResponseDto2.getTitle());
+        assertThat(postList.get(1).getTitle()).isEqualTo(postResponseDto1.getTitle());
     }
 
     @Test
@@ -59,12 +82,50 @@ public class RedisTest {
                 .comment_cnt(0)
                 .build();
 
-        // when
+        PostResponseDto postResponseDto=modelMapper.map(post, PostResponseDto.class);
         postRepository.save(post);
-        postService.findAll();      // 전체 조회하면 캐시에 저장됨
+        Long id=post.getId();
+
+        redisTemplate.opsForZSet().add("findByRank", id.toString(), post.getView());
+        redisTemplate.opsForHash().put("rankByHash", id.toString(), postResponseDto);
+
+        // when
         postService.deletePost(post.getId());
 
         // then
-        assertThat(postService.findAll().size()).isEqualTo(0);
+        assertThat(redisTemplate.opsForHash().hasKey("rankByHash", id.toString())).isEqualTo(false);
+        assertThat(redisTemplate.opsForZSet().size("findByRank")).isEqualTo(0);
+
+    }
+
+    @Test
+    public void redisUpdatePost(){
+        // given
+        Post post=Post.builder()
+                .title("첫 번째")
+                .content("안녕")
+                .author("im")
+                .view(0)
+                .comment_cnt(0)
+                .build();
+
+        postRepository.save(post);
+        PostRequestDto postRequestDto=modelMapper.map(post, PostRequestDto.class);
+        PostResponseDto postResponseDto=modelMapper.map(post, PostResponseDto.class);
+        String id=postResponseDto.getId().toString();
+
+        redisTemplate.opsForHash().put("rankByHash", id, postResponseDto);
+        PostResponseDto hashValue1=(PostResponseDto) redisTemplate.opsForHash().get("rankByHash", id);
+        postRequestDto.setTitle("첫 번째(수정)");
+        postResponseDto.setTitle("첫 번째(수정)");
+
+        // when
+        PostResponseDto afterPostResponseDto=postService.update(Long.valueOf(id), postRequestDto);          // 수정
+
+        // then
+        PostResponseDto hashValue2=(PostResponseDto) redisTemplate.opsForHash().get("rankByHash", id);
+        assertThat(hashValue1.getTitle()).isNotEqualTo(hashValue2.getTitle());
+        assertThat(afterPostResponseDto.getTitle()).isEqualTo("첫 번째(수정)");
+
     }
 }
