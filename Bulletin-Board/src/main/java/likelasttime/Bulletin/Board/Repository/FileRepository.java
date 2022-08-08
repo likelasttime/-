04aa607f01fa2ff62e2d96341ea253dfa3b9369a
KangMinjeong <a50070863@gmail.com>
2022-08-08 23:01:00 +0900

    :ambulance: Repository 수정
    Entity 이름을 UploadFile로 바꿔서 Repository에도 반영

--- Bulletin-Board/src/main/java/likelasttime/Bulletin/Board/Repository/FileRepository.java
+++ Bulletin-Board/src/main/java/likelasttime/Bulletin/Board/Repository/FileRepository.java
@@ -1,8 +1,8 @@
 package likelasttime.Bulletin.Board.Repository;
 
-import likelasttime.Bulletin.Board.domain.posts.File;
+import likelasttime.Bulletin.Board.domain.posts.UploadFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FileRepository extends JpaRepository<File, Long> {
+public interface FileRepository extends JpaRepository<UploadFile, Long> {
 
 }
