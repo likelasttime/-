8c3c6ed0211cacf4a9b2037b41ddaf070e44c016
KangMinjeong <a50070863@gmail.com>
2022-09-03 20:17:10 +0900

    :sparkles: 댓글 수정, 삭제 후 비동기로 댓글 리스트 출력
    비동기 방식으로 댓글 리스트를 출력해서 효율을 높임

--- Bulletin-Board/src/main/java/likelasttime/Bulletin/Board/Controller/CommentController.java
+++ Bulletin-Board/src/main/java/likelasttime/Bulletin/Board/Controller/CommentController.java
@@ -2,7 +2,6 @@ package likelasttime.Bulletin.Board.Controller;
 
 import likelasttime.Bulletin.Board.Service.CommentService;
 import likelasttime.Bulletin.Board.Service.PostService;
-import likelasttime.Bulletin.Board.domain.posts.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -54,30 +53,22 @@ public class CommentController {
 
     // 수정
     @PutMapping("/update/{id}")
-    @ResponseBody
-    public Result update_comment(@PathVariable(value="id") Long comment_id,
-                                 @RequestBody Map<String, String> map){
-        Result result=new Result();
+    public String update_comment(@PathVariable(value="id") Long comment_id,
+                                 @RequestBody Map<String, String> map, Model model){
         String comment=map.get("comment");
-        if (comment.isBlank()){
-            result.setBlank(true);
-            return result;
-        }else if (comment.length() > 10000) {
-            result.setMax(true);
-            return result;
-        }
-        result.setBlank(false);
-        result.setMax(false);
+        Long post_id=Long.parseLong(map.get("post_id"));
         commentService.update(comment_id, comment);
-        return result;
+        model.addAttribute("commentList", postService.getCommentList(post_id));
+        return "/post/detail :: #commentTable";
     }
 
     // 삭제
     @PostMapping("/delete/{id}")
-    public String delete(@PathVariable("id") Long id, @RequestBody Map<String, String> map) {
+    public String delete(@PathVariable("id") Long id, @RequestBody Map<String, String> map, Model model) {
         Long post_id=Long.parseLong(map.get("post_id"));        // 게시글 번호
         commentService.delete(id, post_id);
-        return "redirect:/post/detail/" + post_id;
+        model.addAttribute("commentList", postService.getCommentList(post_id));
+        return "/post/detail :: #commentTable";
     }
 
 
