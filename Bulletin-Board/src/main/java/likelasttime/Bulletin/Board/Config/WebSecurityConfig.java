1fe4d9e06586a03d94b5b2ea2c19580e29ad3e26
KangMinjeong <a50070863@gmail.com>
2022-09-28 23:36:20 +0900

    :wrench: AuthenticationManager 빈 추가
    소셜 로그인시 인증 처리하기 위해 필요
    "/oauth2/**"로 바꾼 이유는 "/oauth2/a/b"도 매핑할 수 있어 유연함

--- Bulletin-Board/src/main/java/likelasttime/Bulletin/Board/Config/WebSecurityConfig.java
+++ Bulletin-Board/src/main/java/likelasttime/Bulletin/Board/Config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package likelasttime.Bulletin.Board.Config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,7 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .csrfTokenRepository(new CookieCsrfTokenRepository())
                 .and()
                 .authorizeRequests()
-                    .antMatchers("/", "/user/availability/**", "/oauth2/*").permitAll()
+                    .antMatchers("/", "/user/availability/**", "/oauth2/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -61,5 +62,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
